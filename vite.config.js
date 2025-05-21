46f2181fe68ad340bc1793a51c9bc502184726ee
mkgdevelopers <muaaznaeemoff@gmail.com>
2025-05-21 16:02:38 +0500

    configured vercel

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base : process.env.VITE_BASE_PATH || "/vista-appointment",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
